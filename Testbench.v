f14d052f1baf4f8b626f0f7255bc2654e8ac441b
Ritav <ritav08092004@gmail.com>
2026-05-13 20:39:02 +0530

    Update Testbench.v

--- Testbench.v
+++ Testbench.v
@@ -1,26 +1,7 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 13.05.2026 19:21:27
-// Design Name: 
-// Module Name: tb_sdFSM1011
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-module tb_sdFSM1011(
-
-    );
+module tb_sdFSM1011();
+   reg data_ti;
+   reg rst_ti;
+   reg clk_ti;
+   wire dout_to;
+    //pending
 endmodule
