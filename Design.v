2e0528c05f4372f0324bbee7315a501dececdf83
Ritav <ritav08092004@gmail.com>
2026-05-13 20:22:58 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -6,7 +6,7 @@ module sdFSM1011(
 );
 
    //net(s)
-   reg present_state;
+   reg [1:0]present_state;
 
    //States
    localparam 
@@ -16,41 +16,46 @@ module sdFSM1011(
       S3 = 2'b11;
 
    //FSM
-   always@(posedge rst_i) begin
-      present_state <= S0;
-   end
-
-   always@(posedge clk_i) begin
+   always@(posedge rst_i, posedge clk_i) begin
+      //Reset
+      if(rst_i) begin
+         present_state <= S0;
+         dout_o <= 1'b0;
+      end
       //State Transitions
-      case(present_state)
-         S0: begin 
-            if(data_i) present_state <= S1;
-         end
-         S1: begin 
-            if(data_i == 1'b0) present_state <= S2;
-         end
-         S2: begin 
-            if(data_i) present_state <= S3; 
-            else present_state <= S0;
-         end
-         S3: begin 
-            if(data_i) present_state <= S1;
-            else present_state <= S2; 
-         end
-         default: present_state <= S0;
-      endcase
+      else begin
+         present_state <= S0; //default
+         case(present_state)
+            S0: begin 
+               if(data_i) present_state <= S1;
+            end
+            S1: begin 
+               if(data_i == 1'b0) present_state <= S2;
+            end
+            S2: begin 
+               if(data_i) present_state <= S3; 
+               else present_state <= S0;
+            end
+            S3: begin 
+               if(data_i) present_state <= S1;
+               else present_state <= S2; 
+            end
+            default: ;
+         endcase
       
-      //Output Generation
-      case(present_state)
-         S0: dout_o <= 1'b0;
-         S1: dout_o <= 1'b0;
-         S2: dout_o <= 1'b0;
-         S3: begin 
-            if(data_i) dout_o <= 1'b1; 
-            else dout_o <= 1'b0;
-         end
-         default: dout_o <= 1'b0;
-      endcase
+         //Output Generation
+         dout_o <= 1'b0; //default
+         case(present_state)
+            S0: dout_o <= 1'b0;
+            S1: dout_o <= 1'b0;
+            S2: dout_o <= 1'b0;
+            S3: begin 
+               if(data_i) dout_o <= 1'b1; 
+               else dout_o <= 1'b0;
+            end
+            default: ;
+         endcase
+      end
    end
    
 endmodule
