b48bef72a54f40a862118ed40efee1ce13f074de
Ritav <ritav08092004@gmail.com>
2026-05-29 18:50:04 +0530

    Indentation

--- Testbench.v
+++ Testbench.v
@@ -1,37 +1,38 @@
 module tb_sdFSM1011();
-   reg data_ti;
-   reg rst_ti;
-   reg clk_ti;
+   reg  data_ti;
+   reg  rst_ti;
+   reg  clk_ti;
    wire dout_to;
 
 //reg(s)
-reg [3:0]d = 4'b0;
+reg [3:0] d = 4'b0;
 
 //instantiation
-sdFSM1011 DUT(.data_i(data_ti), 
-              .clk_i (clk_ti) ,
-              .rst_i (rst_ti) ,
-              .dout_o(dout_to));
+sdFSM1011 DUT(.data_i (data_ti), 
+              .clk_i  (clk_ti) ,
+              .rst_i  (rst_ti) ,
+              .dout_o (dout_to)
+);
 
 //clock
 initial begin
-   clk_ti = 1'b0;
-   forever #5 clk_ti = ~clk_ti;
+              clk_ti = 1'b0;
+   forever #5 clk_ti = ~ clk_ti;
 end
 
 //Feeding
 initial begin
    //Reset
-      rst_ti = 1'b1;
-      data_ti = 1'b0;
-      #3 rst_ti = 1'b0;
+         rst_ti  = 1'b1;
+         data_ti = 1'b0;
+      #3 rst_ti  = 1'b0;
    //Data
-      #3 data_ti = 1'b0;
+                #3  data_ti = 1'b0;
       repeat(5) #10 data_ti = $urandom_range(0,1);
-      #10 data_ti = 1'b1;
-      #10 data_ti = 1'b0;
-      #10 data_ti = 1'b1;
-      #10 data_ti = 1'b1;
+                #10 data_ti = 1'b1;
+                #10 data_ti = 1'b0;
+                #10 data_ti = 1'b1;
+                #10 data_ti = 1'b1;
       repeat(8) #10 data_ti = $urandom_range(0,1);
 end //feeding
 
@@ -42,7 +43,8 @@ end
 
 //capture
 initial begin
-   $monitor("Time: %0t, Clk: %b, Rst: %b, IN: %0b, OUT: %b", $time, clk_ti, rst_ti, data_ti, dout_to);
+   $monitor ("Time: %0t, Clk: %b, Rst: %b, IN: %0b, OUT: %b", 
+             $time,      clk_ti,  rst_ti, data_ti, dout_to);
    $dumpfile("sdFSM1011.vcd");
    $dumpvars(0, tb_sdFSM1011);
 end
